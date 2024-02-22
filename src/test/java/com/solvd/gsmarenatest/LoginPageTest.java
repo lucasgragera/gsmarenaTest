4650d6cebc033f3197a46d868cfce7f24c73289b
Lucas Gragera <lucasgragera1@gmail.com>
2024-02-22 20:34:52 -0300

    gsmarenaTest

---
+++ src/test/java/com/solvd/gsmarenatest/LoginPageTest.java
@@ -0,0 +1,25 @@
+package com.solvd.gsmarenatest;
+
+import com.zebrunner.carina.core.AbstractTest;
+import com.zebrunner.carina.core.IAbstractTest;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import static com.zebrunner.agent.core.webdriver.RemoteWebDriverFactory.getDriver;
+import static org.testng.Assert.assertTrue;
+
+
+public class LoginPageTest implements IAbstractTest {
+    @Test
+    public void verifyLoginTest(){
+        LoginPage loginPage = new LoginPage(getDriver());
+
+
+        loginPage.open();
+        loginPage.typeEmail("lucasgragera51@gmail.com");
+        loginPage.typePassword("jdsfhds");
+        loginPage.clickLoginButton();
+        assertTrue(loginPage.isErrorMessagePresent(), "Error message isn't present");
+
+    }
+}
